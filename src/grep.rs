3c6068d0760b68eb1f255ef1d4975631cf07ed7d
Thorsten Reichert <th.reichert@gmx.net>
2025-09-20 11:34:26 +0200

    add \w matching

--- src/grep.rs
+++ src/grep.rs
@@ -1,12 +1,12 @@
 pub mod patterns;
 
-use patterns::is_digit;
-
 pub fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
     } else if pattern == "\\d" {
-        return input_line.contains(is_digit)
+        return input_line.contains(patterns::is_digit);
+    } else if pattern == "\\w" {
+        return input_line.contains(patterns::is_word);
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
@@ -34,5 +34,15 @@ mod tests {
     #[test]
     fn test_match_pattern_digit_no_digit() {
         assert!(!match_pattern("abcdefg", "\\d"))
+    }
+
+    #[test]
+    fn test_match_pattern_word() {
+        assert!(match_pattern("fool101", "\\w"))
+    }
+
+    #[test]
+    fn test_match_pattern_word_no_word() {
+        assert!(!match_pattern("$!?", "\\w"))
     }
 }
--- src/grep/patterns.rs
+++ src/grep/patterns.rs
@@ -1,5 +1,21 @@
+fn is_in_range(lower_inclusive: char, upper_inclusive: char, char: char) -> bool {
+    lower_inclusive <= char && char <= upper_inclusive
+}
+
 pub fn is_digit(char: char) -> bool {
-    return '0' <= char && char <= '9';
+    is_in_range('0', '9', char)
+}
+
+pub fn is_lower_case_letter(char: char) -> bool {
+    is_in_range('a', 'z', char)
+}
+
+pub fn is_upper_case_letter(char: char) -> bool {
+    is_in_range('A', 'Z', char)
+}
+
+pub fn is_word(char: char) -> bool {
+    is_digit(char) || is_lower_case_letter(char) || is_upper_case_letter(char) || char == '_'
 }
 
 #[cfg(test)]
@@ -25,5 +41,78 @@ mod tests {
         assert!(!is_digit('a'));
         assert!(!is_digit('%'));
         assert!(!is_digit('_'));
+    }
+
+    #[test]
+    fn test_is_lower_case_letter() {
+        assert!(is_lower_case_letter('a'));
+        assert!(is_lower_case_letter('q'));
+        assert!(is_lower_case_letter('z'));
+    }
+
+    #[test]
+    fn test_is_lower_case_letter_upper_case_letter() {
+        assert!(!is_lower_case_letter('A'));
+        assert!(!is_lower_case_letter('Q'));
+        assert!(!is_lower_case_letter('Z'));
+    }
+
+    #[test]
+    fn test_is_lower_case_letter_digit() {
+        assert!(!is_lower_case_letter('3'))
+    }
+
+    #[test]
+    fn test_is_lower_case_letter_symbol() {
+        assert!(!is_lower_case_letter('_'))
+    }
+
+    #[test]
+    fn test_is_upper_case_letter() {
+        assert!(is_upper_case_letter('A'));
+        assert!(is_upper_case_letter('Q'));
+        assert!(is_upper_case_letter('Z'));
+    }
+
+    #[test]
+    fn test_is_upper_case_letter_upper_case_letter() {
+        assert!(!is_upper_case_letter('a'));
+        assert!(!is_upper_case_letter('q'));
+        assert!(!is_upper_case_letter('z'));
+    }
+
+    #[test]
+    fn test_is_upper_case_letter_digit() {
+        assert!(!is_upper_case_letter('3'))
+    }
+
+    #[test]
+    fn test_is_upper_case_letter_symbol() {
+        assert!(!is_upper_case_letter('_'))
+    }
+
+    #[test]
+    fn test_is_word_lower_case_letter() {
+        assert!(is_word('d'))
+    }
+
+    #[test]
+    fn test_is_word_upper_case_letter() {
+        assert!(is_word('G'))
+    }
+
+    #[test]
+    fn test_is_word_digit() {
+        assert!(is_word('7'))
+    }
+
+    #[test]
+    fn test_is_word_underscore() {
+        assert!(is_word('_'))
+    }
+
+    #[test]
+    fn test_is_word_other_symbol(){
+        assert!(!is_word('$'))
     }
 }
