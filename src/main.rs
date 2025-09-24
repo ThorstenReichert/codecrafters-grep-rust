802990f6d6bd7100bba6beb7353355f3ee57af5f
Thorsten Reichert <th.reichert@gmx.net>
2025-09-24 20:41:22 +0200

    extract matching from stdin into own function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,19 @@ mod grep;
 
 use grep::match_pattern;
 
+fn grep_stdin(pattern: &str) -> i32 {
+    let mut input_line = String::new();
+
+    io::stdin().read_line(&mut input_line).unwrap();
+
+    // Uncomment this block to pass the first stage
+    if match_pattern(&input_line, &pattern) {
+        0
+    } else {
+        1
+    }
+}
+
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     if env::args().nth(1).unwrap() != "-E" {
@@ -14,14 +27,6 @@ fn main() {
     }
 
     let pattern = env::args().nth(2).unwrap();
-    let mut input_line = String::new();
-
-    io::stdin().read_line(&mut input_line).unwrap();
-
-    // Uncomment this block to pass the first stage
-    if match_pattern(&input_line, &pattern) {
-        process::exit(0)
-    } else {
-        process::exit(1)
-    }
+    
+    process::exit(grep_stdin(&pattern))
 }
